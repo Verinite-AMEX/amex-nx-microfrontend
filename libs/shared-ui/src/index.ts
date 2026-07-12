0d18a8445335529bae6450c0c49f4a4c5c73d9fc
Logeshwaran S <V00683@VERINITE1.local>
2026-07-12 17:38:52 +0530

    Remove unused shared-ui/shared-utils libs, clean tsconfig paths & ui-components moved to root level

--- libs/shared-ui/src/index.ts
+++
@@ -1,3 +0,0 @@
-// Export all shared UI components, directives, and pipes from this barrel.
-// Add exports here as you create shared components used by multiple micro-frontends.
-export * from './lib/shared-ui.module';
--- libs/shared-utils/src/index.ts
+++
@@ -1,5 +0,0 @@
-// Export all shared utilities (services, guards, interceptors, models, tokens)
-// that are consumed by two or more micro-frontend applications.
-export * from './lib/shared-utils.module';
-export * from './lib/auth-token.interceptor';
-export * from './lib/shell.token';
