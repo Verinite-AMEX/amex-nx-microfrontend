407934ea87612724dec3a49a704ba5649cf860bc
sachinwaghmareverinite <sachin.waghmare@verinite.com>
2026-07-16 12:04:55 +0530

    removed the commented code

--- apps/vat_invoice/src/app/pages/vat-invoice-report/vat-invoice-report.ts
+++ apps/vat_invoice/src/app/pages/vat-invoice-report/vat-invoice-report.ts
@@ -1,38 +1,3 @@
-// import { CommonModule } from '@angular/common';
-// import { Component, EventEmitter, Input, Output } from '@angular/core';
-
-// import { ButtonComponent, AmexPaginatedTableComponent } from '@ui-components/ui';
-
-// import { VatInvoice } from '../../models/vat-invoice.model';
-
-// @Component({
-//   selector: 'app-vat-invoice-report',
-//   standalone: true,
-//   imports: [CommonModule, ButtonComponent, AmexPaginatedTableComponent],
-//   templateUrl: './vat-invoice-report.html',
-//   styleUrl: './vat-invoice-report.css',
-// })
-// export class VatInvoiceReport {
-//   @Input() invoice: VatInvoice | null = null;
-
-//   @Output() downloadPDF = new EventEmitter<void>();
-
-//   get totalVat(): number {
-//     if (!this.invoice) {
-//       return 0;
-//     }
-
-//     return this.invoice.lineItems.reduce(
-//       (total, item) => total + item.vatAmount,
-//       0,
-//     );
-//   }
-
-//   downloadPdf(): void {
-//     this.downloadPDF.emit();
-//   }
-// }
-
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
